fcb9dfbfee8da73b4580d759fa1c6a8a724c1299
Nam Nguyen <nhnkg123@gmail.com>
2022-01-11 15:35:09 +0700

    refactor(lab01): ES - Operator

--- lab01/demo05.js
+++ lab01/demo05.js
@@ -11,9 +11,21 @@ console.log("num = num / 3 =>", num / 3);
 console.log("num = num % 3 =>", num % 3);
 console.log("num = num / 3 =>", parseInt(num / 3));
 
+num  = 100;
 console.log("num++ =>", num++);
 console.log("num", num)
 console.log("++num =>", ++num);
 console.log("num-- =>", num--);
 console.log("--num =>", --num);
 //num++ in num ra trước mới cộng sau
+
+num  = 100;
+
+num += 5;
+console.log("num += 5", num);
+num -= 4;
+console.log("num -= 4", num);
+num *= 2;
+console.log("num *= 2", num);
+num /= 6;
+console.log("num /= 6", num);
