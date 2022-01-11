b21d65602873904aa05e8f12dc3c91f2d9aa6f79
Nam Nguyen <nhnkg123@gmail.com>
2022-01-11 15:31:22 +0700

    feature(lab01): ES - Operator

---
+++ lab01/demo05.js
@@ -0,0 +1,19 @@
+/**
+ * Các phép toán và phép so sánh
+ */
+
+let num = 100;
+console.log("num = num + 1 =>", num + 1);
+console.log("num = num - 1 =>", num - 1);
+console.log("num = num * 2 =>", num * 2);
+console.log("num = num / 2 =>", num / 2);
+console.log("num = num / 3 =>", num / 3);
+console.log("num = num % 3 =>", num % 3);
+console.log("num = num / 3 =>", parseInt(num / 3));
+
+console.log("num++ =>", num++);
+console.log("num", num)
+console.log("++num =>", ++num);
+console.log("num-- =>", num--);
+console.log("--num =>", --num);
+//num++ in num ra trước mới cộng sau
