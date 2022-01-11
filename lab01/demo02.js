afb92901712873a5e019c20bd8ce5aa4ff564cc1
Nam Nguyen <nhnkg123@gmail.com>
2022-01-11 11:00:48 +0700

    feature(lab01): ES - data type

---
+++ lab01/demo02.js
@@ -0,0 +1,30 @@
+/**
+ * ES - kiểu dữ liệu 
+ */
+
+let num = 1;
+let float_num = 1.25;
+
+console.log("Number - Interger:", num);
+console.log("Number_float", float_num);
+
+let string = "Hello world";
+console.log ("String:", string);
+
+let object = {
+    id: 1,
+    name: 'Product 01',
+    price: 1000
+};
+console.log('Object:', object);
+
+let array = [1, 2, 3, 4, 5]
+console.log ("Array:", array);
+
+let undefine_obj;  //chưa có kiểu cố định 
+console.log("undefine:", undefine_obj);
+
+let null_obj = 100;
+console.log ("null_object:", null_obj);
+null_obj = null;
+console.log ("null_object:", null_obj);
