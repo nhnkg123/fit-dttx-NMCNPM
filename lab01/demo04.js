61c9652b1c393010e53b0fc7c058801172f616c9
Nam Nguyen <nhnkg123@gmail.com>
2022-01-11 15:18:59 +0700

    feature(lab01): ES - compare variable

---
+++ lab01/demo04.js
@@ -0,0 +1,15 @@
+/**
+ * So sánh dữ liệu 
+ */
+
+console.log("1 == 1 =>",1 == 1);
+console.log("1.0 == 1 =>",1.0 == 1);
+console.log("1 == '1'=>",1 == '1');
+console.log("1.0 == '1'=>",1.0 == '1');
+console.log ("-------------------------");
+console.log("1 === 1 =>",1 === 1);
+console.log("1.0 === 1 =>",1.0 === 1);
+console.log("1 === '1'=>",1 === '1');
+console.log("1.0 === '1'=>",1.0 === '1');
+// ba dấu bằng thì kiểu số và string sẽ khác nhau là false
+// nghĩa là khi dùng (strictly) === thì giống nhau về chuỗi thì phải giống kiểu dữ liệu mới true
